bc66a1bf10e6d4882e5800f6781861ff5b6e53a4
Weronika Materna <wmaterna8@gmail.com>
2021-02-09 13:53:55 +0100

    Initial Commit

---
+++ RealmPlantsShop/RealmPlantsShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  RealmPlantsShopApp.swift
+//  RealmPlantsShop
+//
+//  Created by Weronika Materna on 09/02/2021.
+//
+
+import SwiftUI
+
+@main
+struct RealmPlantsShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
